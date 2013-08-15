751d85cc4d614d5d3b2ccf4444f036e20724be59
Olibear <oliver@math.umd.edu>
2013-08-14 22:54:27 -0400

    Better Graph Setup
    Forgot to update references.

--- src/arl/graph/util/CommonAlgorithms.java
+++ src/arl/graph/util/CommonAlgorithms.java
@@ -1,6 +1,7 @@
 package arl.graph.util;
 
 import arl.core.Arc;
+import arl.core.Edge;
 import arl.core.Link;
 import arl.core.Graph;
 import arl.core.Route;
@@ -18,13 +19,13 @@ public class CommonAlgorithms {
 	 * @return a Route object containing the tour.
 	 * @throws IllegalArgumentException if the graph passed in is not Eulerian.
 	 */
-	public static Route tryFleury(DirectedGraph<DirectedVertex, Arc<DirectedVertex>> eulerianGraph) throws IllegalArgumentException{
+	public static Route tryFleury(DirectedGraph<Arc> eulerianGraph) throws IllegalArgumentException{
 		if (!isEulerian(eulerianGraph))
 			throw new IllegalArgumentException();
 		//TODO: Fleury's
 		return fleury(eulerianGraph);
 	}
-	public static Route tryFleury(UndirectedGraph<UndirectedVertex, Link<UndirectedVertex>> eulerianGraph) throws IllegalArgumentException{
+	public static Route tryFleury(UndirectedGraph<Edge> eulerianGraph) throws IllegalArgumentException{
 		if (!isEulerian(eulerianGraph))
 			throw new IllegalArgumentException();
 		return fleury(eulerianGraph);
@@ -69,7 +70,7 @@ public class CommonAlgorithms {
 	{
 		return false;
 	}
-	public static boolean isEulerian (DirectedGraph<DirectedVertex, Arc<DirectedVertex>> graph)
+	public static boolean isEulerian (DirectedGraph<Arc> graph)
 	{
 		for(DirectedVertex v: graph.getVertices())  
 		{
@@ -78,7 +79,7 @@ public class CommonAlgorithms {
 		}
 		return true;
 	}
-	public static boolean isEulerian(UndirectedGraph<UndirectedVertex, Link<UndirectedVertex>> graph) 
+	public static boolean isEulerian(UndirectedGraph<Edge> graph) 
 	{
 		for (UndirectedVertex v:graph.getVertices()) 
 		{
